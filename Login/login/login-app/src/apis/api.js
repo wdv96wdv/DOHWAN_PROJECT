ee30e74f474cc808c746dce6a41e681994f8fb6a
wdv96wdv <58497627+wdv96wdv@users.noreply.github.com>
2025-10-12 06:58:31 +0900

    api 제거

--- Login/login/login-app/src/apis/api.js
+++ Login/login/login-app/src/apis/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://dohwan-project.onrender.com/api";
+const API_BASE_URL = "https://dohwan-project.onrender.com";
 // axios 객체 생성
 const api = axios.create({
   baseURL: API_BASE_URL,
