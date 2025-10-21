ba63a922c538ee5015cc67b6c164af1b4926e580
wdv96wdv <58497627+wdv96wdv@users.noreply.github.com>
2025-10-22 03:49:30 +0900

    살짝수정

--- Login/login/login-app/src/pages/admin/AdminContact.jsx
+++ Login/login/login-app/src/pages/admin/AdminContact.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import styles from "../../assets/css/Common.module.css";
+import styles from "../../assets/css/common.module.css";
 import Swal from "sweetalert2"; 
 
 
