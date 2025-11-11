befa5d3ad2695083cef4ecfca985c5502f27bfd7
wdv96wdv <58497627+wdv96wdv@users.noreply.github.com>
2025-11-11 22:45:22 +0900

    RH->Rh

--- Login/login/login-app/src/pages/Login/Home.jsx
+++ Login/login/login-app/src/pages/Login/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "../../assets/css/Home.module.css";
-import RecommendRH from "../../assets/img/RecommendRH.jpg";
+import RecommendRh from "../../assets/img/RecommendRh.jpg";
 import marathon from "../../assets/video/marathon.mp4";
 import banner1 from "../../assets/img/banner1.jpg";
 import banner2 from "../../assets/img/banner2.jpg";
@@ -201,8 +201,8 @@ const Home = () => {
 </p>
         <button className={styles.ctaBtn} onClick={() => navigate("/recommend")}>러닝화 추천받기</button>
       </div>
-      <div className={styles.recommendRH}>
-  <img src={RecommendRH} alt="RecommendRH" className={styles.recommendRHImg} />
+      <div className={styles.recommendRh}>
+  <img src={RecommendRh} alt="RecommendRh" className={styles.recommendRhImg} />
   <p className={styles.imageCredit}>
     <a
       href="https://kr.freepik.com/free-photo/sporty-man-training-outdoors-london_12750620.htm"
